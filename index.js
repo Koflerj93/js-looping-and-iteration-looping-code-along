e7518635fff1e2fb1f0d1e8a0942bdccf8f47a41
Koflerj93 <koflerj93@gmail.com>
2020-11-16 20:46:47 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (name, message) {
+    const newArr = [];
+    for (let i = 0; i < name.length; i++) {
+      newArr.push(`Thank you, ${name[i]}, for the wonderful ${message} gift!`)
+    }
+    return newArr;
+  }
+
+
+function countDown(num) {
+    let counter = num
+     while (counter >= 0 ) {
+         console.log(counter);
+         counter--
+     }
+    }
